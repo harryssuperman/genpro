1bb4b21b0713531631cedbfcc53703598a0aaca5
harryssuperman <harryssuperman@hotmail.com>
2020-11-22 02:32:21 +0100

    ADD: Maven wrapper and first classes

---
+++ src/main/java/com/github/harryssuperman/genpro/GenproApplication.java
@@ -0,0 +1,13 @@
+package com.github.harryssuperman.genpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/harryssuperman/genpro/GenproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.harryssuperman.genpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
